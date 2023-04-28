ac2a77986bece20d4d78630e52e012256d6a3e6e
Ashuakhira <bashok592@gmail.com>
2023-04-28 14:50:13 +0530

    Branch5 is added

---
+++ RollDice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class RollDice
+    {
+        public static void call()
+        {
+            Console.WriteLine("start the snake and ladder game");
+            Console.WriteLine("single player at start position 0");
+            Random random = new Random();
+            int RollDice = random.Next(1, 6);
+            Console.WriteLine("player 1 rolls the die and get:" + RollDice);
+        }
+    }
+}
