2e799265cf581c5fa737e8ea45fa2310efda14ee
Ashuakhira <bashok592@gmail.com>
2023-04-28 14:43:03 +0530

    Branch1 added

---
+++ SnakeAndLadderProblem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SnakeAndLadderProblem
+    {
+        public static void Call()
+        {
+            Console.WriteLine("start the snake and ladder Game");
+            Console.WriteLine("Single player at position 0");
+        }
+    }
+}
