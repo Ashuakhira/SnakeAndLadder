db7dfa69e2e1f3e52d46fb6310d23d050477429f
Ashuakhira <bashok592@gmail.com>
2023-04-28 14:40:09 +0530

    File added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+            // SnakeAndLadderProblem.Call();
+            //RollDice.call();
+            // RolldiceLadderGame.call();
+            //SnakeAndLadderRolldice.call();
+            //WinningPosition.call();
+           // RollCount.call();
+            GameWith2Player.call();
+
+        }
+    }
+}
